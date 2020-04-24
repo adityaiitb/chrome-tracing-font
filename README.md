6d3d319b297d222ae54d6fe99afb0985521bedb9
Aditya Agrawal <aditya.iitb@gmail.com>
2020-04-23 23:52:53 -0700

    Added README, javascript code and images.

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # chrome-tracing-font
-Adjust the font and track height in chrome tracing
+
+Adjust the font and track height in chrome tracing.
+
+Chrome tracing `chrome://tracing` is an amazing tool to visualize timeline
+(performance) information. It is used by Javascript developers, all
+major Deep Learning frameworks e.g. TensorFlow, PyTorch, MXNet etc.
+
+However, the font size in the tool is notoriously small (10 pt) which puts
+a lot of strain on the eyes. Browser scaling does not really help because 
+the labels are actually inside a HTML `canvas` element and not part of the
+DOM.
+
+The javascipt code in `adjust_font_height.js` allows you to adjust the
+font of the labels and the height of the tracks. A track is a row of
+colored rectangles.
+
+## Instructions
+- Load a trace in `chrome://tracing`.
+- Open Developer tools using `Shift`+`Ctrl`+`I` or `F12`.
+- Go to the Console tab.
+- Paste the code in `adjust_font_height.js`.
+
+Optionally, adjust the font and track height before pasting.
+
+## Result
+[Before][./before.png] [After][./after.png]
+
+## Known Issues
+The code does not play nice when tracks are collapsed/expanded. Solutions
+are welcome.
---
+++ adjust_font_height.js
@@ -0,0 +1,64 @@
+// Adjust these values.
+var labelFont = 25;
+var trackHeight = 30;
+
+function adjustTrackHeight(trackHeight) {
+	var elems = document.getElementsByClassName("track rect-track");
+	for (var i = 0; i < elems.length; i++) {
+		elems[i].style.cssText += "height: " + trackHeight + "px;";
+	}
+}
+
+adjustTrackHeight(trackHeight);
+
+tr.ui.b.drawLabels = function(ctx, dt, viewLWorld, viewRWorld, slices, async, fontSize, yOffset) {
+    const elidedTitleCache = new tr.ui.b.ElidedTitleCache();
+    const ColorScheme = tr.b.ColorScheme;
+    const colorsAsStrings = ColorScheme.colorsAsStrings;
+    const EventPresenter = tr.ui.b.EventPresenter;
+    const blackColorId = ColorScheme.getColorIdForReservedName('black');
+    const THIN_SLICE_HEIGHT = 4;
+    const SLICE_WAITING_WIDTH_DRAW_THRESHOLD = 3;
+    const SLICE_ACTIVE_WIDTH_DRAW_THRESHOLD = 1;
+    const SHOULD_ELIDE_TEXT = true;
+    const pixelRatio = window.devicePixelRatio || 1;
+    const pixWidth = dt.xViewVectorToWorld(1);
+    ctx.save();
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'top';
+    //ctx.font = (fontSize * pixelRatio) + 'px sans-serif';
+    ctx.font = labelFont + 'px sans-serif';
+    if (async) {
+        ctx.font = 'italic ' + ctx.font;
+    }
+    const cY = yOffset * pixelRatio;
+    const lowSlice = tr.b.findLowIndexInSortedArray(slices, function(slice) {
+        return slice.start + slice.duration;
+    }, viewLWorld);
+    const quickDiscardThreshold = pixWidth * 20;
+    for (let i = lowSlice; i < slices.length; ++i) {
+        const slice = slices[i];
+        if (slice.start > viewRWorld)
+            break;
+        if (slice.duration <= quickDiscardThreshold)
+            continue;
+        const xLeftClipped = Math.max(slice.start, viewLWorld);
+        const xRightClipped = Math.min(slice.start + slice.duration, viewRWorld);
+        const visibleWidth = xRightClipped - xLeftClipped;
+        const title = slice.title + (slice.didNotFinish ? ' (Did Not Finish)' : '');
+        let drawnTitle = title;
+        let drawnWidth = elidedTitleCache.labelWidth(ctx, drawnTitle);
+        const fullLabelWidth = elidedTitleCache.labelWidthWorld(ctx, drawnTitle, pixWidth);
+        if (SHOULD_ELIDE_TEXT && fullLabelWidth > visibleWidth) {
+            const elidedValues = elidedTitleCache.get(ctx, pixWidth, drawnTitle, drawnWidth, visibleWidth);
+            drawnTitle = elidedValues.string;
+            drawnWidth = elidedValues.width;
+        }
+        if (drawnWidth * pixWidth < visibleWidth) {
+            ctx.fillStyle = EventPresenter.getTextColor(slice);
+            const cX = dt.xWorldToView((xLeftClipped + xRightClipped) / 2);
+            ctx.fillText(drawnTitle, cX, cY, drawnWidth);
+        }
+    }
+    ctx.restore();
+}
